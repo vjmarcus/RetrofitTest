56b4b9e0b81a134b38ede1ed998cd237fca6d166
vjmarcus <vjmarcus1@gmail.com>
2019-10-26 16:27:12 +0300

    Add EmployeeAdapter

---
+++ app/src/main/java/com/freshappbooks/retrofittest/adapters/EmployeeAdapter.java
@@ -0,0 +1,62 @@
+package com.freshappbooks.retrofittest.adapters;
+
+import android.view.LayoutInflater;
+import android.view.View;
+import android.view.ViewGroup;
+import android.widget.TextView;
+
+import androidx.annotation.NonNull;
+import androidx.recyclerview.widget.RecyclerView;
+
+import com.freshappbooks.retrofittest.R;
+import com.freshappbooks.retrofittest.pojo.Employee;
+
+import java.util.List;
+
+public class EmployeeAdapter extends RecyclerView.Adapter<EmployeeAdapter.EmployeeViewHolder> {
+
+    private List <Employee> employees;
+
+    public List<Employee> getEmployees() {
+        return employees;
+    }
+
+    public void setEmployees(List<Employee> employees) {
+        this.employees = employees;
+        notifyDataSetChanged();
+    }
+
+    @NonNull
+    @Override
+    public EmployeeViewHolder onCreateViewHolder(@NonNull ViewGroup parent, int viewType) {
+        View view = LayoutInflater.from(parent.getContext()).inflate(R.layout.employee_item, parent, false);
+        return new EmployeeViewHolder(view);
+    }
+
+    @Override
+    public void onBindViewHolder(@NonNull EmployeeViewHolder holder, int position) {
+        Employee employee = employees.get(position);
+        holder.textViewName.setText(employee.getName());
+        holder.textViewLastName.setText(employee.getLastName());
+    }
+
+    @Override
+    public int getItemCount() {
+        return employees.size();
+    }
+
+    class EmployeeViewHolder extends RecyclerView.ViewHolder {
+
+        private TextView textViewName;
+        private TextView textViewLastName;
+
+
+
+        public EmployeeViewHolder(@NonNull View itemView) {
+            super(itemView);
+
+            textViewName = itemView.findViewById(R.id.textView_name);
+            textViewLastName = itemView.findViewById(R.id.textView_lastname);
+        }
+    }
+}
