ac4a09507aa7f00165adf0a37b5321adaa577392
vjmarcus <vjmarcus1@gmail.com>
2019-10-26 16:49:46 +0300

    Add ApiFactory

--- app/src/main/java/com/freshappbooks/retrofittest/MainActivity.java
+++ app/src/main/java/com/freshappbooks/retrofittest/MainActivity.java
@@ -1,14 +1,31 @@
 package com.freshappbooks.retrofittest;
 
 import androidx.appcompat.app.AppCompatActivity;
+import androidx.recyclerview.widget.LinearLayoutManager;
+import androidx.recyclerview.widget.RecyclerView;
 
 import android.os.Bundle;
 
+import com.freshappbooks.retrofittest.adapters.EmployeeAdapter;
+import com.freshappbooks.retrofittest.pojo.Employee;
+
+import java.util.ArrayList;
+import java.util.List;
+
 public class MainActivity extends AppCompatActivity {
+
+    private RecyclerView recyclerView;
+    private EmployeeAdapter adapter;
 
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_main);
+
+        recyclerView = findViewById(R.id.recycler_view_employees);
+        adapter = new EmployeeAdapter();
+        recyclerView.setLayoutManager(new LinearLayoutManager(this));
+        recyclerView.setAdapter(adapter);
+
     }
 }
