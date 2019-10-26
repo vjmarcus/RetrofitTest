51ed678cef3acf585eed7ead6dbf2f80da57caaa
vjmarcus <vjmarcus1@gmail.com>
2019-10-26 16:06:30 +0300

    Add Specialty class

---
+++ app/src/main/java/com/freshappbooks/retrofittest/pojo/Specialty.java
@@ -0,0 +1,31 @@
+package com.freshappbooks.retrofittest.pojo;
+
+import com.google.gson.annotations.Expose;
+import com.google.gson.annotations.SerializedName;
+
+public class Specialty {
+
+    @SerializedName("specialty_id")
+    @Expose
+    private int specialtyId;
+    @SerializedName("name")
+    @Expose
+    private String name;
+
+    public int getSpecialtyId() {
+        return specialtyId;
+    }
+
+    public void setSpecialtyId(int specialtyId) {
+        this.specialtyId = specialtyId;
+    }
+
+    public String getName() {
+        return name;
+    }
+
+    public void setName(String name) {
+        this.name = name;
+    }
+
+}
