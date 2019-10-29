51417811858809d761d9a01f29049f0962514d8e
vjmarcus <vjmarcus1@gmail.com>
2019-10-29 20:00:26 +0300

    Add new dependency

--- app/src/main/java/com/freshappbooks/retrofittest/pojo/Employee.java
+++ app/src/main/java/com/freshappbooks/retrofittest/pojo/Employee.java
@@ -1,10 +1,12 @@
 package com.freshappbooks.retrofittest.pojo;
 
+import androidx.room.Entity;
+
 import com.google.gson.annotations.Expose;
 import com.google.gson.annotations.SerializedName;
 
 import java.util.List;
-
+@Entity
 public class Employee {
 
     @SerializedName("f_name")
